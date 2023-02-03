c3d9b4cbdcc36bc02548b47a04a8148d2eef31c9
Serhii Kryvorotenko <serhii@MacBook-Pro-Serhii.local>
2023-02-03 13:37:38 +0100

    finish

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,26 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
 import Swiper, {Navigation, Pagination} from 'swiper';
+Swiper.use([Pagination]);
 
-const swiper = new Swiper();
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
+const swiper2 = new Swiper('.swiper-2', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween:40,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    breakpoints: {
+        420: {slidesPerView: 4,}
+    }
+});
 
 
 let menu = document.getElementById('menu');
